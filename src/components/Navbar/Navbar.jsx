00654841879baccd9549ea00f9eae16ab3bdd38f
deividtrindade777 <silvadeivi321@gmail.com>
2026-06-17 22:27:15 -0300

    refatoração navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,21 +3,21 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        🐾 PetGuardian
-      </div>
+    <>
+      <aside className="sidebar">
+        <h2>🐾 PetGuardian</h2>
 
-      <div className="nav-links">
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/pets">Pets</Link>
-        <Link to="/vacinas">Vacinas</Link>
-        <Link to="/consultas">Consultas</Link>
-        <Link to="/medicamentos">Medicamentos</Link>
-        <Link to="/banhotosa">Banho/Tosa</Link>
-        <Link to="/perfilpet">Perfil</Link>
-      </div>
-    </nav>
+        <nav>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/pets">Pets</Link>
+          <Link to="/vacinas">Vacinas</Link>
+          <Link to="/consultas">Consultas</Link>
+          <Link to="/medicamentos">Medicamentos</Link>
+          <Link to="/banhotosa">Banho e Tosa</Link>
+          <Link to="/perfilpet">Perfil do Pet</Link>
+        </nav>
+      </aside>
+    </>
   );
 }
 
