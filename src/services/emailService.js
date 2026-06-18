fc8fa1420b7f5713bcd9a79048ff4e486074f265
deividtrindade777 <silvadeivi321@gmail.com>
2026-06-18 03:59:00 -0300

    refatoração geral

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,28 +1,22 @@
 import emailjs from "@emailjs/browser";
 
 export async function enviarEmail(
-  email,
+  emailDestino,
   nome,
   tipo,
   mensagem,
   dataEvento
 ) {
-  try {
-    await emailjs.send(
-      "service_9buxba4",
-      "template_w8xjl1k",
-      {
-        email,
-        nome,
-        tipo,
-        mensagem,
-        dataEvento,
-      },
-      "E2ajjFRuo90roVvTn"
-    );
-
-    console.log("E-mail enviado!");
-  } catch (error) {
-    console.error(error);
-  }
+  await emailjs.send(
+    "service_9buxba4",
+    "template_w8xjl1k",
+    {
+      to_email: emailDestino,
+      nome,
+      tipo,
+      mensagem,
+      dataEvento,
+    },
+    "E2ajjFRuo90roVvTn"
+  );
 }
--- src/services/notificacoesService.js
+++ src/services/notificacoesService.js
@@ -3,25 +3,27 @@ import { auth, db } from "./firebase";
 import { enviarEmail } from "./emailService";
 
 export async function criarNotificacao({
-  petId,
-  tipo,
-  mensagem,
-  dataEvento,
-}) {
-  if (!auth.currentUser) return;
-
-  await addDoc(collection(db, "notificacoes"), {
-    userId: auth.currentUser.uid,
     petId,
     tipo,
     mensagem,
     dataEvento,
-    concluido: false,
-  });
+}) {
+    if (!auth.currentUser) return;
 
-  await enviarEmail(
-    auth.currentUser.email,
-    `PetGuardian - ${tipo}`,
-    `${mensagem}\n\nData do evento: ${dataEvento}`
-  );
+    await addDoc(collection(db, "notificacoes"), {
+        userId: auth.currentUser.uid,
+        petId,
+        tipo,
+        mensagem,
+        dataEvento,
+        concluido: false,
+    });
+
+    await enviarEmail(
+        auth.currentUser.email,
+        auth.currentUser.displayName || "tutor",
+        tipo,
+        mensagem,
+        dataEvento
+    );
 }
