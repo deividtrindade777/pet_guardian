0c9862242fdd11ac3dc935a1cfb7c963985379bb
deividtrindade777 <silvadeivi321@gmail.com>
2026-06-18 04:27:19 -0300

    refatoração

--- src/services/notificacoesService.js
+++ src/services/notificacoesService.js
@@ -3,19 +3,27 @@ import { auth, db } from "./firebase";
 import { enviarEmail } from "./emailService";
 
 export async function criarNotificacao({
+  petId,
+  tipo,
+  mensagem,
+  dataEvento,
+}) {
+  if (!auth.currentUser) return;
+
+  await addDoc(collection(db, "notificacoes"), {
+    userId: auth.currentUser.uid,
     petId,
     tipo,
     mensagem,
     dataEvento,
-}) {
-    if (!auth.currentUser) return;
+    concluido: false,
+  });
 
-    await addDoc(collection(db, "notificacoes"), {
-        userId: auth.currentUser.uid,
-        petId,
-        tipo,
-        mensagem,
-        dataEvento,
-        concluido: false,
-    });
+  await enviarEmail(
+    auth.currentUser.email,
+    auth.currentUser.displayName || "tutor",
+    tipo,
+    mensagem,
+    dataEvento
+  );
 }
