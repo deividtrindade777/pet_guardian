79862d4b6bb1cd6113935d25f02ddeb2228e95cd
deividtrindade777 <silvadeivi321@gmail.com>
2026-06-18 04:17:42 -0300

    refatoração

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,22 +1,28 @@
 import emailjs from "@emailjs/browser";
 
 export async function enviarEmail(
-  emailDestino,
+  email,
   nome,
   tipo,
   mensagem,
   dataEvento
 ) {
-  await emailjs.send(
-    "service_9buxba4",
-    "template_w8xjl1k",
-    {
-      to_email: emailDestino,
-      nome,
-      tipo,
-      mensagem,
-      dataEvento,
-    },
-    "E2ajjFRuo90roVvTn"
-  );
+  try {
+    await emailjs.send(
+      "service_9buxba4",
+      "template_w8xjl1k",
+      {
+        email,
+        nome,
+        tipo,
+        mensagem,
+        dataEvento,
+      },
+      "E2ajjFRuo90roVvTn"
+    );
+
+    console.log("E-mail enviado!");
+  } catch (error) {
+    console.error(error);
+  }
 }
